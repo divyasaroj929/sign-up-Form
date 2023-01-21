f3e653e78a8c97b77c2b753408c40f51d39658c5
Divya <divyasaroj29@gmail.com>
2023-01-21 19:25:16 +0530

    sign up

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/sign-up-form" element={<Home />}></Route>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/login-page" element={<Login />}></Route>
           <Route path="/Submitpage" element={<Submitpage />}></Route>
         </Routes>
