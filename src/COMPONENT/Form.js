6bc9d6b7f4bdb2da107b34d672c610110997bb1d
Divya <divyasaroj29@gmail.com>
2023-07-12 16:30:47 +0530

    add submitPage

--- src/COMPONENT/Form.js
+++ src/COMPONENT/Form.js
@@ -1,7 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Submitpage from "./Submitpage";
-import bubble from "../COMPONENT/bubble.jpg";
 import Login from "./Login";
 import university from "./university.jpg";
 const Form = () => {
